40c4f061ece1d8b45c653b27caee74b4f3eefa1f
VaronDev <varonssmuel6@gmail.com>
2026-08-29 22:52:34 -0500

    Update

--- project/backend/main.py
+++ project/backend/main.py
@@ -1,6 +1,12 @@
 # main.py
+# main.py
 import os
+import sys
 import asyncio
+
+if sys.platform == "win32":
+    asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+
 import logging
 import glob
 
